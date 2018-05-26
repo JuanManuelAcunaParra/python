9649fc73c8ab997b8d3b02480c7ec3c38c999929
JuanManuelAcunaParra <juan.acuna.123@gmail.com>
2018-05-26 12:42:12 -0400

    Primer commit

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+
+
+#! C:\Python27\python.exe
+import os, sys
+a = int(input("Numero 1 :"))
+b = int(input("Numero 2 :"))
+c = int(input("Numero 3 :"))
+d = int(input("Numero 4 :"))
+cadena = ("La sumatoria es:")
+sumatoria = (a+b+c+d)
+print(cadena, sumatoria)
---
+++ suma.py
@@ -0,0 +1,5 @@
+#! C:\Python27\python.exe
+import os, sys
+a = int(input("Unidades :"))
+b = int(input("Precio :"))
+print("A Pagar=", a*b)
